6e417c63e5647c8b5a95934f5fd20f70f2ad8888
Daniel <daniel.burns.2022@student.lhschools.org>
2020-02-20 11:17:22 -0500

    OddnessMethodCreated

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -1,8 +1,23 @@
 package com.LickingHeights;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	testOddness();
+    }
+
+    public static void testOddness() {
+        int number;
+        Scanner keyboard;
+        keyboard = new Scanner(System.in);
+
+        System.out.println("Enter a positive integer.");
+        number = keyboard.nextInt();
+
+        System.out.println("Number is odd: " + (number % 2 != 0));
+
+
     }
 }
